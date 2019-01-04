32b5c85304b08d81b31599454fd31c99ec6a2955
austin <eastin93@gmail.com>
2019-01-04 14:42:34 -0500

    initial commit

---
+++ glib/Observables.hx
@@ -0,0 +1,26 @@
+package glib;
+
+/**
+ * Implements the Observer Pattern as detailed: https://code.haxe.org/category/design-patterns/observer.html
+ */
+interface Observer {
+  public function notified(sender:{}, ?data:Any):Void;
+}
+/**
+ * Implements the Observer Pattern as detailed: https://code.haxe.org/category/design-patterns/observer.html
+ */
+class Observable {
+  private var observers:Array<Observer> = [];
+
+  private function notify<T>(?data:T) {
+    for (obs in observers) obs.notified(this, data);
+  }
+
+  public function add_observer(observer:Observer) {
+    observers.push(observer);
+  }
+
+  public function remove_observer(observer:Observer) {
+    observers.remove(observer);
+  }
+}
