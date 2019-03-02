997d855e635f07c593a024e8fef7107f73df509b
austin <eastin93@gmail.com>
2019-03-01 23:17:36 -0500

    inlined functions

--- glib/Random.hx
+++ glib/Random.hx
@@ -2,16 +2,16 @@ package glib;
 
 class Random {
   /**
-   * Takes a minimum and maximum Float and returns a random value bewteen them,
-   * leave blank to return a value between -1 and 1
+   * Takes a minimum and maximum Float and returns a random value bewteen them.
+   * Leave blank to return a value between -1 and 1
    * @param	min	Minimum desired result
    * @param	max	Maximum desired result
    */
-  public static function range(?min:Float = -1, ?max:Float = 1):Float return min + Math.random() * (max - min);
+  public static inline function range(?min:Float = -1, ?max:Float = 1):Float return min + Math.random() * (max - min);
 
-  public static function range_int(?min:Float = -1, ?max:Float = 1):Int return Std.int(range(min, max));
+  public static inline function range_int(?min:Float = -1, ?max:Float = 1):Int return Std.int(range(min, max));
 
-  public static function range_int_to_string(?min:Float, ?max:Float):String return "" + Math.floor(range_int(min, max));
+  public static inline function range_int_to_string(?min:Float, ?max:Float):String return "" + Math.floor(range_int(min, max));
 
-  public static function chance(percent:Float = 50):Bool return Math.random() < percent / 100;
+  public static inline function chance(percent:Float = 50):Bool return Math.random() < percent / 100;
 }
