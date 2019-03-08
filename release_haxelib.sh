a57a6926447e6d346ca84a5445e260f167440067
austin <eastin93@gmail.com>
2019-03-08 00:08:55 -0500

    travis fix

--- release_haxelib.sh
+++ release_haxelib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f ghostlib.zip
-zip -r ghostlib.zip src *.md *.json *.hxml run.n
+zip -r ghostlib.zip ghost *.md *.json *.hxml run.n
 haxelib submit ghostlib.zip $HAXELIB_PWD --always
